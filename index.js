c097031863bfa2ad8ae6eb21a8b0d190bbbd8104
Patrick Maweu <muilipato@gmail.com>
2023-03-27 10:43:07 +0300

    arrow functions assignment done

---
+++ index.js
@@ -0,0 +1,13 @@
+const divide = (num1, num2) => {
+      num1 =2000;
+      num2 = 100;
+     return num1/num2;
+}
+const square = () => {
+    let x =2;
+    return x*x;
+}
+const add = (a,b) => {
+    return a+b;
+
+}
